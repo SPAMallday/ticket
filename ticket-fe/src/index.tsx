200d4a5502073a7573b742b49a15ec758fedf13b
SPAMallday <pjw090@gmail.com>
2023-07-03 18:33:39 +0900

    ts setting

--- ticket-fe/src/index.tsx
+++ ticket-fe/src/index.tsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline } from "@mui/material";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
