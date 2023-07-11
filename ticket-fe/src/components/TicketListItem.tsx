4170bf41aeee2e66d313e26dfa8c3fd5b221a63c
SPAMallday <pjw090@gmail.com>
2023-07-11 18:22:46 +0900

    [feat] navbar 위치 고정

--- ticket-fe/src/components/TicketListItem.tsx
+++ ticket-fe/src/components/TicketListItem.tsx
@@ -1,36 +1,36 @@
 import { Card, CardContent, Grid, Typography } from "@mui/material";
 
 const card = (
-    <>
-      <CardContent sx={{ py:"0.5rem!important" }}>
-        <Grid container justifyContent={"space-between"}>
-            <Grid>
-                <Typography sx={{fontWeight:700, fontSize:"1.05rem"}} component="div">
-                더클라임
-                </Typography>
-            </Grid>
-            <Grid>
-                <Typography sx={{fontWeight:700, fontSize:"1.05rem"}} component="div">
-                기간권
-                </Typography>
-            </Grid>
+  <>
+    <CardContent sx={{ py:"0.5rem!important" }}>
+      <Grid container justifyContent={"space-between"}>
+        <Grid>
+          <Typography sx={{fontWeight:700, fontSize:"1.05rem"}} component="div">
+          더클라임
+          </Typography>
         </Grid>
-        <Typography sx={{ fontSize:"0.8rem" }} color="text.secondary">
-          2023.03.01 ~ 2023.06.01
-        </Typography>
-        <Typography sx={{ fontSize:"0.8rem" }} color="text.secondary">
-          1개월 : 손해 / 3개월 : 손해
-        </Typography>
-      </CardContent>
-    </>
-  );
+        <Grid>
+          <Typography sx={{fontWeight:700, fontSize:"1.05rem"}} component="div">
+          기간권
+          </Typography>
+        </Grid>
+      </Grid>
+      <Typography sx={{ fontSize:"0.8rem" }} color="text.secondary">
+        2023.03.01 ~ 2023.06.01
+      </Typography>
+      <Typography sx={{ fontSize:"0.8rem" }} color="text.secondary">
+        1개월 : 손해 / 3개월 : 손해
+      </Typography>
+    </CardContent>
+  </>
+);  
 
 export default function TicketListItem() {
-    return (
-        <>
-            <Card variant="outlined" sx={{backgroundColor:"#EC646480", borderRadius:3, mb:0.5}}>
-                {card}
-            </Card>
-        </>
-    );
+  return (
+    <>
+      <Card variant="outlined" sx={{backgroundColor:"#EC646480", borderRadius:3, mb:0.5}}>
+          {card}
+      </Card>
+    </>
+  );
 }
--- ticket-fe/src/routes/Root.tsx
+++ ticket-fe/src/routes/Root.tsx
@@ -4,11 +4,11 @@ import { Outlet } from "react-router-dom";
 
 export default function Root() {
     return (
-        <>  
+        <Box sx={{height:"100vh", display:"flex", flexDirection:"column", justifyContent:"space-between"}}>  
             <Box px={3} pt={2}>
                 <Outlet />
             </Box>
             <NavBar />
-        </>
+        </Box>
     );
 }
