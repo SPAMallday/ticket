9739194cbee61a4814e6bd637c1963ca3cbe957d
SPAMallday <pjw090@gmail.com>
2023-11-21 17:07:29 +0900

    [feat] 캘린더 수정 및 배지 추가

--- ticket-fe/src/components/CustomCalendar.tsx
+++ ticket-fe/src/components/CustomCalendar.tsx
@@ -1,13 +1,14 @@
 import dayjs, { Dayjs } from "dayjs";
 import Badge from "@mui/material/Badge";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { PickersDay, PickersDayProps } from "@mui/x-date-pickers/PickersDay";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
-import { DayCalendarSkeleton } from "@mui/x-date-pickers/DayCalendarSkeleton";
-import { koKR } from "@mui/x-date-pickers";
-import { useEffect, useRef, useState } from "react";
+import { MouseEvent, useEffect, useRef, useState } from "react";
 import Calendar from "react-calendar";
+import { Value } from "react-calendar/dist/cjs/shared/types";
+
+// 달력 CSS
+import "react-calendar/dist/Calendar.css";
+import "./Calendar.css";
+import { Box } from "@mui/material";
 
 function getRandomNumber(min: number, max: number) {
     return Math.round(Math.random() * (max - min) + min);
@@ -62,7 +63,11 @@ export default function CustomCalendar() {
     const [isLoading, setIsLoading] = useState(false);
     const [highlightedDays, setHighlightedDays] = useState([1, 2, 15]);
 
-    const [value, onChange] = useState<Date | null>(new Date());
+    const [focusDate, setFocusDate] = useState<Date | null>(new Date());
+    const handleDateChange = (v: Value, e: MouseEvent<HTMLButtonElement>) => {
+        const temp = new Date(v?.toString()!);
+        setFocusDate(temp);
+    };
 
     const fetchHighlightedDays = (date: Dayjs) => {
         const controller = new AbortController();
@@ -101,57 +106,31 @@ export default function CustomCalendar() {
         fetchHighlightedDays(date);
     };
 
-    // TODO React-calendar로 변경?
+    // TODO 월이 바뀌면 불러올 데이터도 바뀌도록
     return (
-        // <LocalizationProvider
-        //     dateAdapter={AdapterDayjs}
-        //     localeText={
-        //         koKR.components.MuiLocalizationProvider.defaultProps.localeText
-        //     }
-        // >
-        //     <DateCalendar
-        //         defaultValue={initialValue}
-        //         loading={isLoading}
-        //         onMonthChange={handleMonthChange}
-        //         renderLoading={() => <DayCalendarSkeleton />}
-        //         slots={{
-        //             day: ServerDay,
-        //         }}
-        //         slotProps={{
-        //             day: {
-        //                 highlightedDays,
-        //             } as any,
-        //         }}
-        //         views={["day"]}
-        //         sx={{ width: "auto" }}
-        //     />
-        // </LocalizationProvider>
-        <Calendar
-            onChange={onChange} // useState로 포커스 변경 시 현재 날짜 받아오기
-            formatDay={(locale, date) => dayjs(date).format("DD")} // 날'일' 제외하고 숫자만 보이도록 설정
-            value={value}
-            minDetail='month' // 상단 네비게이션에서 '월' 단위만 보이게 설정
-            maxDetail='month' // 상단 네비게이션에서 '월' 단위만 보이게 설정
-            navigationLabel={null}
-            showNeighboringMonth={false} //  이전, 이후 달의 날짜는 보이지 않도록 설정
-            className='mx-auto w-full text-sm border-b'
-            tileContent={({ date, view }) => {
-                // 날짜 타일에 컨텐츠 추가하기 (html 태그)
-                // 추가할 html 태그를 변수 초기화
-                let html = [];
-                // 현재 날짜가 post 작성한 날짜 배열(mark)에 있다면, dot div 추가
-                if (highlightedDays.find((x) => x === dayjs(date).date())) {
-                    html.push(<div className='dot'></div>);
-                }
-                // 다른 조건을 주어서 html.push 에 추가적인 html 태그를 적용할 수 있음.
-                return (
-                    <>
-                        <div className='flex justify-center items-center absoluteDiv'>
-                            {html}
-                        </div>
-                    </>
-                );
-            }}
-        />
+        <Box mt={2} mb={2}>
+            <Calendar
+                locale='ko-KR'
+                calendarType='gregory'
+                onChange={handleDateChange} // useState로 포커스 변경 시 현재 날짜 받아오기
+                formatDay={(locale, date) => dayjs(date).format("D")} // 날'일' 제외하고 숫자만 보이도록 설정
+                value={focusDate}
+                minDetail='month' // 상단 네비게이션에서 '월' 단위만 보이게 설정
+                maxDetail='month' // 상단 네비게이션에서 '월' 단위만 보이게 설정
+                showNeighboringMonth={false} //  이전, 이후 달의 날짜는 보이지 않도록 설정
+                tileContent={({ date, view }) => {
+                    // 날짜 타일에 컨텐츠 추가하기 (html 태그)
+                    // 추가할 html 태그를 변수 초기화
+                    let html = [];
+                    // 현재 날짜가 post 작성한 날짜 배열(mark)에 있다면, dot div 추가
+                    if (highlightedDays.find((x) => x === dayjs(date).date())) {
+                        console.log("dot");
+                        html.push(<div className='dot'></div>);
+                    }
+                    // 다른 조건을 주어서 html.push 에 추가적인 html 태그를 적용할 수 있음.
+                    return <div className='dotDiv'>{html}</div>;
+                }}
+            />
+        </Box>
     );
 }
